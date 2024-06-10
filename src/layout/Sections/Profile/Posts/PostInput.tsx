1201c57eb92624698e265f2013a681205f21bad3
Kristina-Tur <kristina.tur06121955@gmail.com>
2024-06-10 19:37:38 +0300

    profile

--- src/layout/Sections/Profile/Posts/PostInput.tsx
+++ src/layout/Sections/Profile/Posts/PostInput.tsx
@@ -1,10 +1,14 @@
 import React, {ChangeEvent, useState} from 'react';
-import {TextField, Button, Grid, AppBar, Toolbar, IconButton} from '@mui/material';
-import {Typography, Badge} from '@mui/material';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import ImageIcon from '@mui/icons-material/Image';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import styled from "styled-components";
+import Button from '@mui/material/Button';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Badge from '@mui/material/Badge';
 
 export const PostInput = () => {
     const [postContent, setPostContent] = useState('');
@@ -20,36 +24,31 @@ export const PostInput = () => {
 
     return (
         <PostInputWrapper>
-            <AppBar position="static" sx={{
+            <Toolbar sx={{
                 backgroundColor: '#fff',
                 borderRadius: '5px',
                 color: 'text.secondary',
-                boxShadow: 'none'
             }}>
-                <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="logo">
+                <TwitterIcon sx={{mr: '15px'}}/>
+                <Typography variant="h6" sx={{flexGrow: 1}}>
+                    My posts
+                </Typography>
+                <IconButton color="inherit">
+                    <Badge badgeContent={4} color="primary">
                         <TwitterIcon/>
-                    </IconButton>
-                    <Typography variant="h6" style={{flexGrow: 1}}>
-                        My posts
-                    </Typography>
-                    <IconButton color="inherit">
-                        <Badge badgeContent={4} color="primary">
-                            <TwitterIcon/>
-                        </Badge>
-                    </IconButton>
-                    <IconButton color="inherit">
-                        <Badge badgeContent={2} color="primary">
-                            <ImageIcon/>
-                        </Badge>
-                    </IconButton>
-                    <IconButton color="inherit">
-                        <Badge badgeContent={5} color="primary">
-                            <FavoriteBorderIcon/>
-                        </Badge>
-                    </IconButton>
-                </Toolbar>
-            </AppBar>
+                    </Badge>
+                </IconButton>
+                <IconButton color="inherit">
+                    <Badge badgeContent={2} color="primary">
+                        <ImageIcon/>
+                    </Badge>
+                </IconButton>
+                <IconButton color="inherit">
+                    <Badge badgeContent={5} color="primary">
+                        <FavoriteBorderIcon/>
+                    </Badge>
+                </IconButton>
+            </Toolbar>
             <TextField
                 id="outlined-multiline-static"
                 label="New post"
