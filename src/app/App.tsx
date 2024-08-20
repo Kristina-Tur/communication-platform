d843a18d4deca3d392dc45752b78a2f50981a73e
Kristina-Tur <kristina.tur06121955@gmail.com>
2024-08-20 23:09:41 +0300

    login

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,10 +4,10 @@ import {Redirect, Route, Switch} from "react-router-dom";
 import styled from "styled-components";
 import {Messages} from "../features/layout/ui/sections/messages/Messages";
 import {UsersContainer} from "../features/layout/ui/sections/users/UsersContainer";
-import ProfileContainer from "../features/layout/ui/sections/profile/ProfileContainer";
 import HeaderContainer from "../features/layout/ui/header/HeaderContainer";
 import {Login} from "../features/auth/ui/Login";
 import {MessagesContainer} from "../features/layout/ui/sections/messages/MessagesContainer";
+import {ProfileContainer} from "../features/layout/ui/sections/profile/ProfileContainer";
 
 /**/
 function App() {
@@ -18,7 +18,7 @@ function App() {
             <ContentWrapper>
                 <Sidebar/>
                 <Switch>
-                    {/*<Route exact path="/samurai-way-main" render={() => <Redirect to='/profile'/>}/>В этом примере мы используем
+                    {/*<Route exact path="/samurai-way-main" render={() => <Redirect to='/profile/:userId'/>}/>В этом примере мы используем
                     компонент Redirect для перенаправления пользователя на страницу /profile при совпадении пути /. Мы
                     также используем свойство exact, чтобы убедиться, что маршрут / совпадает только с точным путем /.*/}
                     <Route path={'/profile/:userId?'} render={() =>
